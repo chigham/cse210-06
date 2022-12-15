56b9dcd9baa84cce78a788f8a1cf46dd3cf48dd6
chigham <chigham@byui.edu>
2022-12-14 19:01:31 -0700

    Updated, cleaned, and commented for director.

--- honty_mall/director.py
+++ honty_mall/director.py
@@ -2,17 +2,39 @@ from honty_mall.stage import Stage
 from honty_mall.terminal_service import TerminalService
 
 class Director():
+    """
+    The director class guides the game, utilizing the stage and terminal service.
+
+    self._stage: The stage with 3 doors and a hidden car.
+    self._terminal_service: The service that collects input and prints to the console.
+    self._chosen_door: A number [1,2,3] used to determine the first door selected.
+    """
 
     def __init__(self):
+        """
+        The Constructor method for the Director class.
+
+        self: The Director class.
+        """
+
         self._stage = Stage()
         self._terminal_service = TerminalService()
         self._chosen_door = False
-        self._stay_or_switch = False
-
-    def _make_it_or_break_it(self):
-        self._stay_or_switch = self._terminal_service.read_text(f"You chose door {self._chosen_door}. Do you think you chose the correct door, or would you like to switch?\nEnter '{self._chosen_door}' to stick with door #{self._chosen_door}, or 's' to switch doors: ")
 
     def play_game(self):
+        """
+        The action the director takes to conduct the game.
+
+        The stage is presented.
+        The user chooses a door.
+        Another empty door is opened.
+        The stage is again presented.
+        The user is prompted if they want to open the chosen door or switch doors.
+        Updated chosen door is opened, possibly revealing the car.
+        Game over.
+
+        self: The Director class.
+        """
 
         self._stage.present_stage()
 
