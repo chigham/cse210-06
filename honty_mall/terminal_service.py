28fb75c61584841b6da73cdac99d37853c4f0b44
chigham <chigham@byui.edu>
2022-12-14 20:33:21 -0700

    Finished terminal_service.py.

--- honty_mall/terminal_service.py
+++ honty_mall/terminal_service.py
@@ -6,34 +6,15 @@ class TerminalService:
     The responsibility of a TerminalService is to provide input and output operations for the 
     terminal.
     """
-     
-    # Not used in this case
-    def read_text(self, prompt):
-        """Gets text input from the terminal. Directs the user with the given prompt.
-
-        Args: 
-            self (TerminalService): An instance of TerminalService.
-            prompt (string): The prompt to display on the terminal.
-
-        Returns:
-            string: The user's input as text.
-        """
-        return input(prompt)
-
-    # this should be good
-    def read_letter(self, prompt):
-        """Gets input from the terminal. Directs the user with the given prompt.
-
-        Args: 
-            self (TerminalService): An instance of TerminalService.
-            prompt (string): The prompt to display on the terminal.
-
-        Returns:
-            string: The user's input (as a character).
-        """
-        return input(prompt).upper()
     
     def read_number(self, prompt):
+        """
+        Handle user input for the first decision - which door would you like to choose?
+
+        self: The TerminalService class.
+        prompt: The text prompting for input.
+        """
+        
         result = ""
         while type(result) != int or result not in [1, 2, 3]:
             result = input(prompt)
@@ -46,6 +27,14 @@ class TerminalService:
         return result
     
     def to_switch_or_not_to_switch(self, prompt, index):
+        """
+        Handle user input for the second decision - stay with the same door or switch to the other open door?
+
+        self: The TerminalService class.
+        prompt: The text prompting for input.
+        index: The integer [1,2,3] representing the door's index number to the player.
+        """
+        
         result = ""
         while result not in ['s', str(index)]:
             result = input(prompt)
@@ -64,16 +53,3 @@ class TerminalService:
             text (string): The text to display.
         """
         print(text)
-
-    def write_word(self, array):
-        """Displays the given array on the terminal. 
-           The entire word would be on the same line
-
-        Args: 
-            self (TerminalService): An instance of TerminalService.
-            array (array): The array to display.
-        """
-        print()
-        for i in range(len(array)):
-            print(f"{array[i]}", end=" ")
-        print("\n")
